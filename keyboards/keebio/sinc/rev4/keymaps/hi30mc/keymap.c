9d51347a955936817a40680e2ba01f8be7a26c59
HiHi3690 <duimstrajakson@gmail.com>
2025-09-17 01:05:37 -0400

    key changes after getting physical board set up

--- keyboards/keebio/sinc/rev4/keymaps/hi30mc/keymap.c
+++ keyboards/keebio/sinc/rev4/keymaps/hi30mc/keymap.c
@@ -126,7 +126,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             flag[0]    = (HSV){197 * 255 / 360, 63 * 255 / 100, 98 * 255 / 100};
             flag[1]    = (HSV){348 * 255 / 360, 45 * 255 / 100, 96 * 255 / 100};
             flag[2]    = (HSV){0 * 255 / 360, 0 * 255 / 100, 75 * 255 / 100};
-            flag[3]    = (HSV){197 * 255 / 360, 63 * 255 / 100, 98 * 255 / 100};
+            flag[3]    = (HSV){348 * 255 / 360, 45 * 255 / 100, 96 * 255 / 100};
             num_colors = 4;
             break;
         case LESBIAN:
@@ -161,7 +161,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     }
 
     if (host_keyboard_led_state().caps_lock) {
-        rgb_matrix_set_color(17, RGB_RED);
+        rgb_matrix_set_color(28, RGB_RED);
     }
     if (host_keyboard_led_state().num_lock && get_highest_layer(layer_state) == 1) {
         rgb_matrix_set_color(15, RGB_RED);
@@ -176,11 +176,11 @@ void keyboard_post_init_user(void) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_80_with_macro(
     KC_CYLT,          KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,              KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_INS,
-    XXXXXXX, KC_F16 , KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_HOME,
+    XXXXXXX, KC_F16 , KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_MPRV, KC_MPLY, KC_MNXT,
     XXXXXXX, XXXXXXX, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,
     XXXXXXX, XXXXXXX, KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
-    XXXXXXX,LSG(KC_S),KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-    XXXXXXX, KC_CYLR, KC_LCTL, KC_LALT, KC_LGUI, MO(2),   MO(1),   KC_BSPC,                     TG(1),   KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
+    XXXXXXX,LSG(KC_S),    KC_LSFT,      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
+    XXXXXXX, KC_CYLR, MO(2),  KC_LCTL, KC_LGUI,  KC_LALT, KC_BSPC, MO(1),                       KC_SPC,  TG(1),   KC_RALT, KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [1] = LAYOUT_80_with_macro(
     _______,          _______, KC_F13 , KC_F14 , KC_F15 , KC_F16 , KC_F17 , KC_F18 ,            KC_F19 , KC_F20 , KC_F21 , KC_F22 , KC_F23 , KC_F24 , _______, _______,
@@ -188,7 +188,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, KC_PPLS, _______,            _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, _______, _______,            _______, _______, _______, _______, _______, _______,          _______, _______,
     _______, _______, _______,          KC_KP_1, KC_KP_2, KC_KP_3, _______, _______,            _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, KC_KP_0, KC_PDOT, MO(1),   KC_PENT,                     TG(1),   TG(2)  , _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, KC_KP_0, KC_PDOT, KC_PENT, MO(1),                       TG(2),   TG(1)  , _______, _______, _______, _______, _______, _______
   ),
   [2] = LAYOUT_80_with_macro(
    _______ ,         _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______, _______, _______,
@@ -196,7 +196,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    RM_SPDU, RM_SPDD, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______, _______, _______, _______,
    _______, _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______,          _______, _______,
    _______, _______, _______,          _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______, _______, _______,
-   QK_BOOT, _______, _______, _______, _______, MO(2),   _______, _______,                     KC_LRST, TG(2),   _______, _______, _______, _______, _______, _______
+   QK_BOOT, NK_TOGG, MO(2),   _______, _______, _______, _______, _______,                     TG(2),   KC_LRST, _______, _______, _______, _______, _______, QK_BOOT
   ),
 //[N] = LAYOUT_80_with_macro( //BLANK SET
 // _______,          _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______, _______, _______,
